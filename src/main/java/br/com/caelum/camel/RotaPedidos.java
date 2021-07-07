5ae2cc1e635e5d1c5bac654b64133f520e957037
RenattoAlencar <alencar.renato@yahoo.com.br>
2021-07-07 15:48:34 -0300

    feat(convert file xml to json)

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -13,10 +13,12 @@ public class RotaPedidos {
 		context.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
-				from("file:pedidos?delay=5s&noop=true")
+				from("file:pedidos?delay=5s&noop=true") //Diretoio entrada dos arquivos.
 						.log("${id}")
-						.marshal().xmljson().log(" ${body}")
-						.to("file:saida");
+						.marshal().xmljson() //converter para xml para json.
+						.log(" ${body}")
+						.setHeader("CamelFileName", simple("${file:name.noext}.json")) //Saida do arquivo em json, ( noext ) - tira a extenção na entrada do arquivo.
+						.to("file:saida"); // Diretorio saida dos arquivos.
 			}
 		});
 
