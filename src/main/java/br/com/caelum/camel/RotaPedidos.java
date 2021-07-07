78fe41fa8cbb017fec972f4506bbfa34815e9584
RenattoAlencar <alencar.renato@yahoo.com.br>
2021-07-07 17:57:16 -0300

    feat(aplication split and filter output file)

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -14,9 +14,19 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5s&noop=true") //Diretoio entrada dos arquivos.
+
+						.split()
+							.xpath("/pedido/itens/item") // separando os itens dentro do xml
+							.log("${id}") //Verificar se está pegando os itens separados por split
+							.log("${body}") //Verificar o conteudo do split
+
+						// Agora o filter deverá ser de acordo com o body
+						.filter() //fitrando o item
+							.xpath("/item/formato[text()='EBOOK']")// filtrando no xml os pedidos que contem itens = 'EBOOK'.
+
 						.log("${id}")
 						.marshal().xmljson() //converter para xml para json.
-						.log(" ${body}")
+						.log(" -- FORMAT JSON ${body}")
 						.setHeader("CamelFileName", simple("${file:name.noext}.json")) //Saida do arquivo em json, ( noext ) - tira a extenção na entrada do arquivo.
 						.to("file:saida"); // Diretorio saida dos arquivos.
 			}
